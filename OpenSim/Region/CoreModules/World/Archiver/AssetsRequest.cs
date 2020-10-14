173b81382395eb5dd652ad7cabacf4b442893093
UbitUmarov <ajlduarte@sapo.pt>
2020-10-14 14:37:41 -0700

    fix oar assets save

--- OpenSim/Region/CoreModules/World/Archiver/AssetsRequest.cs
+++ OpenSim/Region/CoreModules/World/Archiver/AssetsRequest.cs
@@ -129,7 +129,7 @@ namespace OpenSim.Region.CoreModules.World.Archiver
                 return;
             }
 
-            m_timeOutTimer = new System.Timers.Timer(60000);
+            m_timeOutTimer = new System.Timers.Timer(90000);
             m_timeOutTimer .AutoReset = false;
             m_timeOutTimer.Elapsed += OnTimeout;
             m_timeout = false;
