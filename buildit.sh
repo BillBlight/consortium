7b4800acd610f77d57db9b1f6ac3afafaecfa9db
UbitUmarov <ajlduarte@sapo.pt>
2020-10-17 06:06:58 -0700

    added the clean aot to the buildit.sh

--- buildit.sh
+++ buildit.sh
@@ -26,6 +26,22 @@ find . -name "*.mdb" -type f -print0 | xargs -0 /bin/rm -f
 find . -name "*.bak" -type f -print0 | xargs -0 /bin/rm -f
 find . -name "*.obj" -type f -print0 | xargs -0 /bin/rm -rf
 find . -name "*obj" -type d -print0 | xargs -0 /bin/rm -rf
+cd bin
+rm Nini.dll.so
+rm DotNetOpen*.dll.so
+rm Ionic.Zip.dll.so
+rm Newtonsoft.Json.*.dll.so
+rm C5.dll.so
+rm CSJ2K.dll.so
+rm Npgslq.dll.so
+rm RestSharp.dll.so
+rm Mono*.dll.so
+rm MySql*.dll.so
+rm OpenMetaverse*.dll.so
+rm OpenSim*.dll.so
+rm OpenSim*.exe.so
+rm Robust*.exe.so
+cd ..
 
 echo "Running Prebuild"
 ./runprebuild.sh
